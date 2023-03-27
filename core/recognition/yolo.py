d0ba8536ae01cc9b3cb3aef23ef8ea85f0cff98a
Vladislav <57713513+v-like-engine@users.noreply.github.com>
2023-03-28 00:58:01 +0300

    Add comments and details to yolo

--- core/recognition/yolo.py
+++ core/recognition/yolo.py
@@ -1,8 +1,12 @@
 from ultralytics import YOLO
 
 
-stream = 'rtmp://rtmp.klpkw.one/live/test'
+# any stream, any video, any image can be used for recognition on-the-go
+stream = 'rtmp://rtmp.example.com/live/test'
 video = '../../sample_videos/kianu.mp4'
 model = YOLO('yolov8m.pt')
 
 model.predict(source=video, line_thickness=2, conf=0.5, show=True, save=True)
+# YOLO v8 standard version can recognize 80 classes
+# should be enough for most of scenes recognition, but can be trained additionally
+# or someone's version can be used instead of standard
